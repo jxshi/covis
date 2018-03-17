46c833e2631ad72c37ea1e431e3e7b68139530f7
pdiakumis <pdiakumis@unimelb.edu.au>
2018-03-17 13:45:00 +1000

    :joycat:

--- R/data.R
+++ R/data.R
@@ -3,5 +3,8 @@
 #' A vector containing names of cats.
 #' Some are cereal killers.
 #'
-#' @format Atomic character vector with 3 elements
+#' @format Atomic character vector with 3 elements:
+#' * Walter: White, deaf and awesome. Likes chemistry.
+#' * Mollie: Super smart and kind. Likes nature.
+#' * Dexter: Big and ginger. Likes bouncy balls.
 "cat_names"
